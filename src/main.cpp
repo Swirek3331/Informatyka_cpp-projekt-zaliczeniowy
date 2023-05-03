33f000c301813fd717f28380840653753f429666
Swirek3331 <karolsutkowski21@gmail.com>
2023-05-03 11:07:56 +0100

    Początek opcji

--- src/main.cpp
+++ src/main.cpp
@@ -11,5 +11,10 @@ int main()
 	std::cout << "Grupa losowych liczb calkowitych: ";
 	displayAnArray(numbers);
 	
+	std::cout << "Wybierz metode do posortowania zbioru." << std::endl;
+	std::cout << "Oto mozliwe opcje: " << std::endl;
+	
+	std::cout << "1 - sortowanie babelkowe" << std::endl;
+	
     return 0;
 }
